7edc2579fb6aa0977f66275d7979974d717bfed6
PatrykMazurek <33990246+PatrykMazurek@users.noreply.github.com>
2021-10-12 14:48:42 +0200

    Dodanie plików obsługujących archiwum ZIP

--- src/spider/com/Main.java
+++ src/spider/com/Main.java
@@ -5,11 +5,15 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.Deflater;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
 
         List<Person> personList = new ArrayList<>();
 
@@ -27,7 +31,7 @@ public class Main {
         Path startLocation = f.toPath().toAbsolutePath().getParent();
         System.out.println(startLocation);
 
-/*        File[] listFile = startLocation.toFile().listFiles(new FilenameFilter() {
+        File[] listFile = startLocation.toFile().listFiles(new FilenameFilter() {
             @Override
             public boolean accept(File dir, String name) {
                 if (name.endsWith(".dat")){
@@ -38,15 +42,91 @@ public class Main {
                 return false;
             }
         });
-        for (File fl : listFile){
-            System.out.println(fl);
+//        for (File fl : listFile){
+//            System.out.println(fl);
+//        }
+
+        File[] listFile_1 = Path.of("C:\\Projekty\\test\\").toFile().listFiles();
+        //sendToZIPFile(startLocation, "test.zip", listFile_1);
+
+//        readFromZIPFile(Path.of("C:\\Projekty\\test_1\\"),
+//                    startLocation.resolve("test.zip").toString());
+
+        try {
+//            Files.createDirectory(Path.of("test"));
+            Path tempF =  Files.createTempFile(null, "txt");
+            Files.move(startLocation.resolve("osoby.dat"), Path.of("C:\\Projekty\\test\\\"osoby.dat\""));
+//            Files.copy();
+            System.out.println(tempF.toAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-*/
-        if (Files.exists(startLocation.resolve("osoby.dat"))){
+
+
+/*        if (Files.exists(startLocation.resolve("osoby.dat"))){
             System.out.println("Plik w podanej loklizacji istnieje");
             System.out.println(startLocation.resolve("osoby.dat"));
         }
+*/
+    }
 
+    public static void sendToZIPFile(Path location, String name, File[] files){
+
+        try {
+            FileOutputStream fOut = new FileOutputStream(location.resolve(name).toString());
+            ZipOutputStream zipOut = new ZipOutputStream(fOut);
+            zipOut.setLevel(Deflater.NO_COMPRESSION);
+            zipOut.setMethod(ZipOutputStream.DEFLATED);
+
+            for (File f : files){
+                if (f.isFile()){
+                    System.out.println("Zapisuje do archiwum " + f.getName());
+                    ZipEntry ze = new ZipEntry(f.getName());
+                    zipOut.putNextEntry(ze);
+
+                    FileInputStream fin = new FileInputStream(f);
+                    byte[] buffer = new byte[2048];
+                    int length = 0;
+                    while ( (length = fin.read( buffer )) >= 0 ){
+                        zipOut.write(buffer, 0, length);
+                    }
+                    zipOut.closeEntry();
+                }
+            }
+            zipOut.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static void readFromZIPFile(Path destLocation, String zipFile ){
+        try {
+            FileInputStream fInput = new FileInputStream(zipFile);
+            ZipInputStream zInput = new ZipInputStream( fInput );
+            ZipEntry ze;
+            while ( (ze = zInput.getNextEntry()) != null){
+                System.out.println("Nazwa " + ze.getName() + " wielkość " + ze.getSize() );
+                FileOutputStream fOutput =
+                        new FileOutputStream(destLocation.resolve(ze.getName()).toString() );
+                byte[] buffer = new byte[2048];
+                int length = 0;
+                while ( (length = zInput.read( buffer) ) >= 0 ){
+                    fOutput.write(buffer, 0, length);
+                }
+                fOutput.close();
+                zInput.closeEntry();
+            }
+            zInput.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void readFromFileObject(String fileName){
